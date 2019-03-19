08a4d3c133313bcb14c18649c29b3c3ae04cc206
Aarti Jaripatke <aartijaripatke@Aartis-Air.fios-router.home>
2019-03-19 18:34:04 -0400

    Added attributes

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default["PARAG_Challenge"]["sites"]["www.paragjaripatke.com"] = { "port" => 443, "servername" => "www.paragjaripatke.com", "serveradmin" => "jaripatkeparag@hotmail.com" }
--- recipes/apache.rb
+++ recipes/apache.rb
@@ -2,6 +2,11 @@ package "httpd" do
   action :install
 end
 
+node["lamp_stack"]["sites"].each do |sitename, data|
+  document_root = "/var/www/html/#{sitename}"
+end
+
 service "httpd" do
   action [:enable, :start]
 end
+
