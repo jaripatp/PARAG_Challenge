b393abf0b85e79e82558d55ad8a7b12c68f87cb8
Aarti Jaripatke <aartijaripatke@Aartis-Air.fios-router.home>
2019-03-19 19:09:57 -0400

    Updated configs

---
+++ apache.rb
@@ -0,0 +1,9 @@
+node["PARAG_Challenge"]["sites"].each do |sitename, data|
+  document_root = "/var/www/html/#{sitename}"
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+end
