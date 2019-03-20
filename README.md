88074f7a4b8bf1e7957caeb59a027eba00d1b25b
Aarti Jaripatke <aartijaripatke@Aartis-Air.fios-router.home>
2019-03-20 14:01:01 -0400

    Done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PARAG_Challenge
 SED Challenge
+URL to target = https://comcasttg-f55382a182ee1533.elb.us-east-1.amazonaws.com/
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,1 @@
-default["PARAG_Challenge"]["sites"]["www.paragjaripatke.com"] = { "port" => 443, "servername" => "www.paragjaripatke.com", "serveradmin" => "jaripatkeparag@hotmail.com" }
+default["PARAG_Challenge"]["sites"]["www.paragjaripatke.com"] = { "port" => 8080, "servername" => "www.paragjaripatke.com", "serveradmin" => "jaripatkeparag@hotmail.com" }
